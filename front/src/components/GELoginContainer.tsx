676803208373fa3847f6347288fccb0407ec47a2
Daniel <fiend.of.sound@gmail.com>
2022-11-10 14:25:51 +0400

    feat: add files

---
+++ front/src/components/GELoginContainer.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const GELoginContainer = styled.div`
+  overflow: hidden;
+  max-width: 375px;
+  width: 100%;	    
+`
+
+export default GELoginContainer
---
+++ front/src/components/GELoginFormContainer.tsx
@@ -0,0 +1,19 @@
+
+import styled from 'styled-components'
+
+const GELoginFormContainer = styled.div`
+	position: relative;
+  z-index: 1;
+  width: 100%;
+  min-height: 375px;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  border-radius: 2px;
+  padding: 32px;
+  background-color: rgb(255, 255, 255);
+`
+export default GELoginFormContainer
